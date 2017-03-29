ad51d4b84e93819daec923dbb1d6a6f21f1f185f
Duncan Hall <duncan.hall@students.olin.edu>
2017-03-29 13:22:56 -0400

    done with permutations in graphs day02

--- graphs/day02/src/Permutations.java
+++ graphs/day02/src/Permutations.java
@@ -1,10 +1,40 @@
-import java.util.List;
+import java.util.*;
 
 public class Permutations {
 
     public static List<List<Integer>> permutations(List<Integer> A) {
-        // TODO
-        return null;
+
+        ArrayList<Integer> remaining = new ArrayList<>(A);
+        ArrayList<List<Integer>> solutions = new ArrayList<>();
+
+        permutationsHelper(new Stack<Integer>(), remaining, solutions);
+
+        return solutions;
+    }
+
+    private static void permutationsHelper(
+            Stack<Integer> soFar, ArrayList<Integer> remaining, ArrayList<List<Integer>> solutions
+    ) {
+        // base case
+        if (remaining.isEmpty()) {
+            // creating of a new list so soFar can be further modified
+            solutions.add(new ArrayList<>(soFar));
+            return;
+        }
+
+        // if nums remaining, add and recurse with each
+        int n;
+        for (int i = 0; i < remaining.size(); i++) {
+            n = remaining.get(i);
+
+            soFar.push(n);
+            remaining.remove(new Integer(n));  // necessary to look for the object, not the index
+            permutationsHelper(soFar, remaining, solutions);
+
+            // backtrace
+            remaining.add(i, n);
+            soFar.pop();
+        }
     }
 
 }
