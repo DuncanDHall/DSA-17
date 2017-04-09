4a3d2676a2441e8db771ef608af94c8c3460b7eb
Duncan Hall <duncan.hall@students.olin.edu>
2017-04-09 18:34:28 -0400

    greedy/IsFailingVersion solved, greedy homework complete

--- greedy/src/FirstFailingVersion.java
+++ greedy/src/FirstFailingVersion.java
@@ -1,7 +1,23 @@
 public class FirstFailingVersion {
 
     public static long firstBadVersion(long n, IsFailingVersion isBadVersion) {
-        // TODO
-        return -1;
+
+        return firstBadVersionHelper(1, n, isBadVersion);
+    }
+
+    private static long firstBadVersionHelper(long lower, long upper, IsFailingVersion isBadVersion) {
+
+        if (lower == upper) {
+            return lower;
+        }
+
+        long split = (lower + upper) / 2;
+
+        if (isBadVersion.isFailingVersion(split)) {
+            return firstBadVersionHelper(lower, split, isBadVersion);
+        } else {
+            return firstBadVersionHelper(split + 1, upper, isBadVersion);
+        }
+
     }
 }
