cf844205740234db6604632ea8d51cb6aea6ba81
Duncan Hall <duncan.hall@students.olin.edu>
2017-04-12 15:26:47 -0400

    first pass at DP/day00/splitcoins

--- DP/day00/src/SplitCoins.java
+++ DP/day00/src/SplitCoins.java
@@ -1,7 +1,61 @@
 public class SplitCoins {
 
+    /** MEMO */
+    static int[][] memo;
+
+    //CONSTANTS
+
     public static int splitCoins(int[] coins) {
+
+        /** INITIALIZE THE MEMO */
+        int sum = 0;
+        for (int c : coins) sum += c;
+        memo = new int[sum][coins.length];
+        for (int i = 0; i < sum; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
+        // SET GLOBAL CONSTANTS
+        int diff = 0;  // unsigned difference between piles A and B
+        int p = 0;
+
+        // CALL RECURSIVE FUNCTION ON PROBLEM THAT YOU WANT TO SOLVE
+        return recursiveFunction(diff, p, coins, memo);
+
         // TODO
-        return -1;
+    }
+
+
+    private static int recursiveFunction(int diff, int p, int[] coins, int[][] memo){
+
+        // BASE CASES
+        if (p == coins.length) return diff;
+
+        /** HAS THIS BEEN MEMOIZED? */
+        if (memo[diff][p] != -1) return memo[diff][p];
+
+        // RECURRENCE RELATION
+        int toA = recursiveFunction(Math.abs(diff + coins[p]),p + 1, coins, memo);
+        int toB = recursiveFunction(Math.abs(diff - coins[p]), p + 1, coins, memo);
+        int best = toA < toB ? toA : toB;
+
+        /** UPDATE THE MEMO */
+        memo[diff][p] = best;
+
+        // RETURN THE ANSWER
+        return best;
+
+    }
+
+    public static void main(String[] args) {
+        int[] coins;
+
+        coins = new int[] {3,1,1,2,5,7};
+        System.out.println(SplitCoins.splitCoins(coins));
+
+        coins = new int[] {10, 8, 5, 4, 3};
+        System.out.println(SplitCoins.splitCoins(coins));
     }
 }
