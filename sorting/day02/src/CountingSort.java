c591d4b03be9ffa35263daafb6229408ce2beae9
Duncan Hall <duncan.hall@students.olin.edu>
2017-04-28 20:07:06 -0400

    done with sorting/day02/counting

--- sorting/day02/src/CountingSort.java
+++ sorting/day02/src/CountingSort.java
@@ -2,12 +2,41 @@ public class CountingSort {
 
     /**
      * Use counting sort to sort positive integer array A.
-     * Runtime: TODO
+     * Runtime: O(rn) (r is the range)
      *
      * k: maximum element in array A
      */
     static void countingSort(int[] A) {
-        // TODO
+        int minSoFar = Integer.MAX_VALUE;
+        int maxSoFar = Integer.MIN_VALUE;
+        int range;
+        int offset;
+
+        for (int n : A) {
+            maxSoFar = n > maxSoFar ? n : maxSoFar;
+            minSoFar = n < minSoFar ? n : minSoFar;
+        }
+
+        range = maxSoFar - minSoFar;
+        offset = minSoFar;
+
+        int[] counts = new int[range + 1];  // initializes to zero
+        for (int n : A) {
+            counts[n - offset] += 1;
+        }
+
+
+        int j = 0;
+        for (int i = 0; i < counts.length; i++) {
+            while (counts[i] > 0) {
+                counts[i] -= 1;
+                A[j++] = i + offset;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CountingSort.countingSort(new int[]{45,36,24,65,26,43});
     }
 
 }
